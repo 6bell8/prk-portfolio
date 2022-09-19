817eda29e5cbd141d80afa2423c7e54f7630d06c
parkgutime <parkgutime@gmail.com>
2022-09-19 16:34:55 +0900

    portfolio

--- js/repositoriesSW.js
+++ js/repositoriesSW.js
@@ -1,60 +1,58 @@
-// const swiperTopNum = $("#repositories .repositoriesBox").find(".swiper-slide");
-// const swiperSubNum = $("#repositories .projectsBox").find(".swiper-slide");
+const swiperTopNum = $("#repositories .repositoriesBox").find(".swiper-slide");
+const swiperSubNum = $("#repositories .projectsBox").find(".swiper-slide");
 
-// const repositories = new Swiper("#repositories .contents .repositoriesBox", {
+const repositories = new Swiper("#repositories .contents .repositoriesBox", {
+  slidesPerView: "auto",
+  spaceBetween: 170,
+  direction: "vertical",
+  centeredSlides: true,
+  loop: true,
+  allowTouchMove: false,
+  speed: 1000,
+});
+
+const project = new Swiper("#repositories .contents .projectsBox", {
+  slidesPerView: "auto",
+  spaceBetween: 330,
+  direction: "vertical",
+  centeredSlides: true,
+  loop: true,
+  allowTouchMove: false,
+  speed: 1000,
+
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+  pagination: {
+    el: "#repositories .pagniation",
+    type: "bullets",
+    clickable: true,
+  },
+  loopedSlides: swiperSubNum.length,
+});
+
+repositories.controller.control = project;
+project.controller.control = repositories;
+
+// new Swiper("#repositories .contents .swiper-container", {
 //   slidesPerView: "auto",
-//   spaceBetween: 100,
+//   spaceBetween: 170,
 //   direction: "vertical",
 //   centeredSlides: true,
-//   loop: true,
-//   allowTouchMove: true,
-//   speed: 1000,
-//   loopedSlides: swiperTopNum.length,
-// });
-
-// const project = new Swiper("#repositories .contents .projectsBox", {
-//   slidesPerView: "auto",
-//   spaceBetween: 130,
-//   direction: "vertical",
-//   centeredSlides: true,
-//   loop: true,
 //   allowTouchMove: false,
 //   speed: 1000,
+//   loop: true,
+//   // mousewheel: true,
 
-//   navigation: {
-//     nextEl: ".next",
-//     prevEl: ".prev",
-//   },
 //   pagination: {
 //     el: "#repositories .pagniation",
 //     type: "bullets",
 //     clickable: true,
 //   },
 
-//   loopedSlides: swiperSubNum.length,
+//   navigation: {
+//     nextEl: ".next",
+//     prevEl: ".prev",
+//   },
 // });
-
-// repositories.controller.control = project;
-// project.controller.control = repositories;
-
-new Swiper("#repositories .contents .swiper-container", {
-  slidesPerView: "auto",
-  spaceBetween: 170,
-  direction: "vertical",
-  centeredSlides: true,
-  allowTouchMove: false,
-  speed: 1000,
-  loop: true,
-  // mousewheel: true,
-
-  pagination: {
-    el: "#repositories .pagniation",
-    type: "bullets",
-    clickable: true,
-  },
-
-  navigation: {
-    nextEl: ".next",
-    prevEl: ".prev",
-  },
-});
